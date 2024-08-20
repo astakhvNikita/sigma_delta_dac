e8087206943ebf7801b45e223b1549cc97b2071b
Никита Астахов <astakhv.nikita@gmail.com>
2024-08-20 22:17:50 +0300

    Добавлен модуль easyaxil

--- hdl/ip/zynq_fpga_dac_top_design/hdl/zynq_fpga_dac_top_design_wrapper.v
+++ hdl/ip/zynq_fpga_dac_top_design/hdl/zynq_fpga_dac_top_design_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Fri Aug 16 18:37:54 2024
-//Host        : DESKTOP-MKJA6P7 running 64-bit major release  (build 9200)
+//Date        : Tue Aug 20 22:06:11 2024
+//Host        : DESKTOP-GTQVNVP running 64-bit major release  (build 9200)
 //Command     : generate_target zynq_fpga_dac_top_design_wrapper.bd
 //Design      : zynq_fpga_dac_top_design_wrapper
 //Purpose     : IP block netlist
